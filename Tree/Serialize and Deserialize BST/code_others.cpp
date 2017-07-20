551f4ebcaee58cd5486784662a1b64dd3f9f4175
yellowsb <798438829@qq.cpm>
2017-07-20 16:05:27 +0800

    code_others.cpp

---
+++ Tree/Serialize and Deserialize BST/code_others.cpp
@@ -0,0 +1,54 @@
+public class Codec {
+    private static final String SEP = ",";
+    private static final String NULL = "null";
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        if (root == null) return NULL;
+        //traverse it recursively if you want to, I am doing it iteratively here
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while (!st.empty()) {
+            root = st.pop();
+            sb.append(root.val).append(SEP);
+            if (root.right != null) st.push(root.right);
+            if (root.left != null) st.push(root.left);
+        }
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    // pre-order traversal
+    public TreeNode deserialize(String data) {
+        if (data.equals(NULL)) return null;
+        String[] strs = data.split(SEP);
+        Queue<Integer> q = new LinkedList<>();
+        for (String e : strs) {
+            q.offer(Integer.parseInt(e));
+        }
+        return getNode(q);
+    }
+    
+	//remove() 和 poll() 方法都是从队列中删除第一个元素（head）
+	//element() 和 peek() 用于在队列的头部查询元素。
+	//offer，add区别：一些队列有大小限制，因此如果想在一个满的队列中加入一个新项，多出的项就会被拒绝。
+	
+	
+    // some notes:
+    //   5
+    //  3 6
+    // 2   7
+    private TreeNode getNode(Queue<Integer> q) { //q: 5,3,2,6,7
+        if (q.isEmpty()) return null;
+        TreeNode root = new TreeNode(q.poll());//root (5)
+        Queue<Integer> samllerQueue = new LinkedList<>();
+        while (!q.isEmpty() && q.peek() < root.val) {
+            samllerQueue.offer(q.poll());
+        }
+        //smallerQueue : 3,2   storing elements smaller than 5 (root)
+        root.left = getNode(samllerQueue);
+        //q: 6,7   storing elements bigger than 5 (root)
+        root.right = getNode(q);
+        return root;
+    }
+}
