31c45713d8c8d8541010da181c08e008a95cd3ef
yellowsb <798438829@qq.cpm>
2017-07-20 19:34:13 +0800

    code.cpp

--- Tree/Binary Tree Inorder Traversal/code.cpp
+++ Tree/Binary Tree Inorder Traversal/code.cpp
@@ -14,20 +14,39 @@ public:
         return traver(root, vec);
     }
     
-    
-    vector<int> traver(TreeNode* root, vector<int>& vec){
-        
+    //递归实现
+    vector<int> traver(TreeNode* root, vector<int>& vec){        
         if(!root){
             return vec;
-        }
-        
+        }        
         traver(root->left, vec);
-        
         vec.push_back(root->val);
-        
         traver(root->right, vec);
         return vec;
     }
+	
+	
+	/*
+	非递归实现但会改变树的结构
+	vector<int> traverse(TreeNode* root, vector<int>& vec) {
+           stack<TreeNode*> s;
+           s.push(root);
+           while(!s.empty()){
+             TreeNode* node = s.top();
+              if(node ->left){
+                  s.push(node ->left);
+                  node ->left = NULL;//防止重复遍历
+              }else{
+                  s.pop();
+                  vec->push(node->val);
+                  if(node->right){
+                     s.push(node->right);
+                  }   
+              } 
+            }
+          return vec;
+    }
+   */
     
     
 };
